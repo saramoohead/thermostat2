82e3094248b079bd8a8f0cff305f2a84091827b0
stacca <caronstace@gmail.com>
2015-04-14 10:20:18 +0100

    lint fix on spec

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,11 +1,11 @@
-describe('Thermostat', function() {
+describe('Thermostat', function () {
 
 var thermostat;
 
-  it('should have a default temperature of 20', function() {
-    thermostat = new Thermostat();
-    expect(thermostat.temp).toEqual(20);
-  });
+    it('should have a default temperature of 20', function () {
+        thermostat = new Thermostat();
+        expect(thermostat.temp).toEqual(20);
+    });
 });
 
 
